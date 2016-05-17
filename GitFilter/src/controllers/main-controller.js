7edf8d3f0ee0df9578eab82402d5c9fe4100875f
marcuscaesar <marcuscaesar@list.ru>
2016-05-17 13:42:01 +0200

    Add files via upload

--- GitFilter/src/controllers/main-controller.js
+++ GitFilter/src/controllers/main-controller.js
@@ -8,28 +8,27 @@ angular.module('angularApplication')
   $scope.allowedUsersCount = $scope.NUM_USERS_PER_PAGE;
   $scope.githubUsersPage = [];
 	
-	$scope.readUsers = function() {
-  	GithubUsersService.getUsers(function(userInfo) {
-  	  if ($scope.githubUsersPage.length < $scope.allowedUsersCount) {
+  $scope.readUsers = function() {
+    GithubUsersService.getUsers(function(userInfo) {
+      if ($scope.githubUsersPage.length < $scope.allowedUsersCount) {
         $scope.githubUsersPage.push(userInfo);
-  	  }
-  	});
-	};
+      }
+    });
+  };
 	
-	$scope.readMoreUsers = function() {
-	  if ($scope.githubUsersPage.length == 0) {
-	    $scope.readUsers();
-	  } else {
-	    $scope.allowedUsersCount += $scope.NUM_USERS_PER_PAGE;
-  	  var lastUserId = 
-  	    $scope.githubUsersPage[$scope.githubUsersPage.length - 1].id;
+  $scope.readMoreUsers = function() {
+    if ($scope.githubUsersPage.length == 0) {
+      $scope.readUsers();
+    } else {
+      $scope.allowedUsersCount += $scope.NUM_USERS_PER_PAGE;
+      var lastUserId = $scope.githubUsersPage[$scope.githubUsersPage.length - 1].id;
       GithubUsersService.getMoreUsers(lastUserId, function(userInfo) {
         if ($scope.githubUsersPage.length < $scope.allowedUsersCount) {
           $scope.githubUsersPage.push(userInfo);
         }
       });
-	  }
-	};
-	
-	$scope.readUsers();
+    }
+  };
+
+  $scope.readUsers();
 }]);
