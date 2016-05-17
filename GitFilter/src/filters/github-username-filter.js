97bfde3e36317c6b6957b320f6d4c9958e1953fb
marcuscaesar <marcuscaesar@list.ru>
2016-05-17 22:59:35 +0200

    Add files via upload

--- GitFilter/src/filters/github-username-filter.js
+++ GitFilter/src/filters/github-username-filter.js
@@ -1,6 +1,17 @@
 angular.module('angularApplication')
 .filter('userName', function() {
-  return function(input, parameterOne) {
-    return input;
+  return function(usersArray, userNameFilterString) {
+    if (userNameFilterString == null || userNameFilterString.length == 0) {
+    	return usersArray;
+    }
+    
+    var filteredArray = [];
+	usersArray.forEach(function(item, itemIndex, itemArray) {
+		if (item.login.indexOf(userNameFilterString) > -1) {
+			filteredArray.push(item);
+		}
+	});
+    
+    return filteredArray;
   }
 });
