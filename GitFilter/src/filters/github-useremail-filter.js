16c1a8911f492e2583a879b5f1220362a568a6fe
marcuscaesar <marcuscaesar@list.ru>
2016-05-18 08:19:00 +0200

    Update github-useremail-filter.js

--- GitFilter/src/filters/github-useremail-filter.js
+++ GitFilter/src/filters/github-useremail-filter.js
@@ -4,14 +4,14 @@ angular.module('angularApplication')
     if (userEmailFilterString == null || userEmailFilterString.length == 0) {
     	return usersArray;
     }
-    
+
     var filteredArray = [];
-	usersArray.forEach(function(item, itemIndex, itemArray) {
-		if (item.email != null && item.email.indexOf(userEmailFilterString) > -1) {
-			filteredArray.push(item);
-		}
-	});
-    
+      usersArray.forEach(function(item, itemIndex, itemArray) {
+        if (item.email != null && item.email.indexOf(userEmailFilterString) > -1) {
+          filteredArray.push(item);
+        }
+    });
+
     return filteredArray;
   }
 });
